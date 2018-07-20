60213cc5f13a102e1164ac387ebfd8597306c564
zhukovka <helen.zhukova@gmail.com>
2018-07-20 21:33:54 +0300

    All in client GraphQL

---
+++ index.js
@@ -0,0 +1,32 @@
+let currentSlide = 0;
+
+const next = document.getElementById("next");
+const slidesContainer = document.getElementById("slidesContainer");
+const slides = slidesContainer.querySelectorAll(".slide");
+
+function switchSlide (to) {
+    slides[currentSlide].style.display = "none";
+    currentSlide = to;
+    slides[to].removeAttribute("style");
+    slidesContainer.removeAttribute("style");
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.target.nodeName === "INPUT") {
+        return;
+    }
+    if (e.code === "ArrowRight") {
+        let to = Math.min(currentSlide + 1, slides.length - 1);
+        switchSlide(to);
+    }
+    if (e.code === "ArrowLeft") {
+        let to = Math.max(currentSlide - 1, 0);
+        switchSlide(to);
+    }
+    if (e.code === "ArrowUp") {
+        switchSlide(currentSlide);
+    }
+    if (next && e.code === "Enter") {
+        open(next.href, "_self");
+    }
+});
